e84479368a528f5783a6a360697caef90d93f037
hrndbrs <hrndbrs@gmail.com>
2025-01-05 23:53:22 +0700

    create database if not exists on up migration

--- db/index.ts
+++ db/index.ts
@@ -1,21 +1,44 @@
+import { Sequelize } from 'sequelize';
 import { Umzug, SequelizeStorage } from 'umzug';
 import dotenv from 'dotenv';
 import getDb from '../src/lib/config/db';
+import type { Dialect } from 'sequelize';
 
 dotenv.config();
 
-const sequelize = getDb();
+async function createDatabase() {
+  const sequelize = new Sequelize({
+    dialect: process.env['DB_DIALECT'] as Dialect,
+    host: process.env['DB_HOST'],
+    username: process.env['DB_USERNAME'],
+    password: process.env['DB_PASSWORD'],
+    port: Number(process.env['DB_PORT']) || 3306,
+  });
 
-const umzug = new Umzug({
-  migrations: {
-    glob: 'db/migrations/*.ts',
-  },
-  context: sequelize.getQueryInterface(),
-  storage: new SequelizeStorage({ sequelize }),
-  logger: console,
-  create: {
-    folder: __dirname + '/migrations',
-  },
-});
+  await sequelize.query(
+    `CREATE DATABASE IF NOT EXISTS ${process.env['DB_NAME']}`,
+    {}
+  );
+}
 
-umzug.runAsCLI();
+(async () => {
+  if (process.argv[2] === 'up') {
+    await createDatabase();
+  }
+
+  const sequelize = getDb();
+
+  const umzug = new Umzug({
+    migrations: {
+      glob: 'db/migrations/*.ts',
+    },
+    context: sequelize.getQueryInterface(),
+    storage: new SequelizeStorage({ sequelize }),
+    logger: console,
+    create: {
+      folder: __dirname + '/migrations',
+    },
+  });
+
+  await umzug.runAsCLI();
+})();
